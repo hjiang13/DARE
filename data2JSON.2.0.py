c72d55bab4ec860d5e5c10b23615e7860a9b1f9f
hjiang13 <hjiang13@kent.edu>
2024-03-31 23:48:18 -0400

    HPC benchmarks as test bench and the data added

---
+++ data2JSON.2.0.py
@@ -0,0 +1,80 @@
+import csv
+import json
+import pandas
+from sklearn.preprocessing import MinMaxScaler
+
+benchPath = ['hackerrank', 'hpc_applications']
+errorType = ['SDC', "benign", "crash"]
+modelType = ["c", "r"]
+
+def getLabel(model, value):
+    if model == "c":
+        if value > 0.75:
+            return 2
+        elif value > 0.5:
+            return 2
+        elif value > 0.25:
+            return 1
+        else:
+            return 1
+    elif value == "r":
+        return value
+
+def getDic(i, df, path, model, error):
+    dictionary = {}
+    print(df['benmark'][i])
+    if path == "hackerrank":
+        codeFile = open(f"{path}/Benchmarks/{df['benmark'][i]}/{df['benmark'][i]}.cpp", 'r')
+        codeLabel = getLabel(model, df[f'{error}'][i])
+        dictionary["code"] = codeFile.read()
+        dictionary["label"] = codeLabel
+        print(codeLabel)
+    elif path == "hpc_applications":
+        codeFile = open(f"{path}/Benchmarks/{df['benmark'][i]}.cpp", 'r')
+        codeLabel = getLabel(model, df[f'{error}'][i])
+        dictionary["code"] = codeFile.read()
+        dictionary["label"] = codeLabel
+
+    return(dictionary)
+
+def getData(model, error, file, path):
+    dataList = []
+    df = pandas.read_csv(file)
+    df_min_max_scaled = df.copy() 
+  
+    # apply normalization techniques by Column 1 
+    column = error
+    df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())     
+  
+    # view normalized data 
+    print(df_min_max_scaled) 
+
+    count_row = df.shape[0]
+    for i in range (0, count_row):
+        dataList.append(getDic(i, df_min_max_scaled, path, model, error))
+    return dataList
+
+
+
+if __name__ == '__main__':
+    for model in modelType:
+        for error in errorType:
+            trainData = []
+            validData = []
+            trainDataFile =  f"./{error}_train_resilience_{model}.jsonl"   
+            for path in benchPath:
+                testData = []
+                testDataFile = f"./{error}_test_resilience_{model}.jsonl"
+                if path == "hackerrank":
+                    csvFile = path+"/errorRate_O0.csv"
+                    trainData = getData(model, error, csvFile, path)
+                else:
+                    csvFile = path + "/errorRate.csv"
+                    testData = getData(model, error, csvFile, path)
+            with open(testDataFile, "w") as outfile:
+                outfile.write("\n".join(json.dumps(i) for i in testData))
+
+            with open(trainDataFile, "w") as outfile:
+                outfile.write("\n".join(json.dumps(i) for i in trainData))
+
+
--- data2JSON.py
+++ data2JSON.py
@@ -3,20 +3,22 @@ import csv
 import json
 import pandas
 from sklearn.preprocessing import MinMaxScaler
-workPath  = "./"
-testDataFile = "./test_resilience.jsonl"
-trainDataFile = "./train_resilience.jsonl"
-validDataFile = "./valid_resilience.jsonl"
 
-csvFile = "hackerrank/errorRate_O0.csv"
+erroType = "SDC"
+workPath  = "./"
+testDataFile = f"./{erroType}_test_resilience.jsonl"
+trainDataFile = f"./{erroType}_train_resilience.jsonl"
+validDataFile = f"./{erroType}_valid_resilience.jsonl"
+csvFile_hackerrank = {"hackerrank/errorRate_O0.csv"}
+csvFile_hpc = "hpc_applications/errorRate.csv"
 ##Classify SDC rate into 4 catogaries by 25%
 def getLabel(SDC_rate):
     if SDC_rate > 0.75:
-        return 4
-    elif SDC_rate > 0.5:
-        return 3
-    elif SDC_rate > 0.25:
         return 2
+    elif SDC_rate > 0.5:
+        return 2
+    elif SDC_rate > 0.25:
+        return 1
     else:
         return 1
 
@@ -24,38 +26,41 @@ def getDic(i, df):
     dictionary = {}
     print(df['benmark'][i])
     codeFile = open(f"hackerrank/Benchmarks/{df['benmark'][i]}/{df['benmark'][i]}.cpp", 'r')
-    codeLabel = getLabel(df['SDC'][i])
+    codeLabel = getLabel(df[f'{erroType}'][i])
     dictionary["code"] = codeFile.read()
     dictionary["label"] = codeLabel
     print(codeLabel)
     return(dictionary)
-
-df = pandas.read_csv(csvFile)
+trainData = []
+testData = []
+validData = []
+#for train data:
+df = pandas.read_csv(csvFile_hackerrank)
 print(df)
 
 df_min_max_scaled = df.copy() 
   
 # apply normalization techniques by Column 1 
-column = 'SDC'
+column = erroType
 df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())     
   
 # view normalized data 
 print(df_min_max_scaled) 
 
-trainData = []
-testData = []
-validData = []
-for i in range(0, 70):
-    trainData.append(getDic(i, df))
-for i in range(70, 86):
-    testData.append(getDic(i, df))
+
+for i in range(0, 86):
+    trainData.append(getDic(i, df_min_max_scaled))
 for i in range(86, 96):
-    validData.append(getDic(i, df))
+    validData.append(getDic(i, df_min_max_scaled))
 
 
 with open(trainDataFile, "w") as outfile:
     outfile.write("\n".join(json.dumps(i) for i in trainData))
-with open(testDataFile, "w") as outfile:
-    outfile.write("\n".join(json.dumps(i) for i in testData))
+
 with open(validDataFile, "w") as outfile:
     outfile.write("\n".join(json.dumps(i) for i in validData))
+
+with open(testDataFile, "w") as outfile:
+    outfile.write("\n".join(json.dumps(i) for i in testData))
+
+#for test data
