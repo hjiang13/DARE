380f9aeacd3a2c81db94b907042dd7a67d4ac353
hjiang13 <hjiang13@kent.edu>
2024-04-09 15:13:40 -0400

    mormalization deleted

--- data2JSON.2.0.py
+++ data2JSON.2.0.py
@@ -17,7 +17,7 @@ def getLabel(model, value):
             return 1
         else:
             return 1
-    elif value == "r":
+    elif model == "r":
         return value
 
 def getDic(i, df, path, model, error):
@@ -43,11 +43,11 @@ def getData(model, error, file, path):
     df_min_max_scaled = df.copy() 
   
     # apply normalization techniques by Column 1 
-    column = error
-    df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())     
+    #column = error
+    #df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())     
   
     # view normalized data 
-    print(df_min_max_scaled) 
+    #print(df_min_max_scaled) 
 
     count_row = df.shape[0]
     for i in range (0, count_row):
