8de089120b405aad9c5893e672e71a61eb96fe3b
hjiang13 <hjiang13@kent.edu>
2024-04-10 18:02:10 -0400

    data normalized

--- data2JSON.2.0.py
+++ data2JSON.2.0.py
@@ -43,8 +43,8 @@ def getData(model, error, file, path):
     df_min_max_scaled = df.copy() 
   
     # apply normalization techniques by Column 1 
-    #column = error
-    #df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())     
+    column = error
+    df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())     
   
     # view normalized data 
     #print(df_min_max_scaled) 
